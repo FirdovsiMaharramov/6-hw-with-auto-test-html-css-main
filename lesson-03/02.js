7989e75e3f02f3a7bcccc5fe747217b513fdbdf9
inam <inamzeynalli01@gmail.com>
2025-02-17 17:12:01 +0400

    first commit

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,8 +1,9 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
-
+let number=90;
+let remainder=(number%2)
 function isEven(int) {
-    if (int%2===0){
+    if (remainder==0){
         return true;
     }
     else{
